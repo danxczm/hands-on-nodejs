cb324bf38a0eebc19cbcbe146fbe66c340aca775
dan <81313913+danxczm@users.noreply.github.com>
2025-07-17 22:24:07 +0300

    feat: methods for mongoos

--- src/server.js
+++ src/server.js
@@ -4,37 +4,56 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllStudents, getStudentById } from './services/students.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
 export const startServer = () => {
-    const app = express();
+  const app = express();
 
-    app.use(express.json());
-    app.use(cors());
+  app.use(express.json());
+  app.use(cors());
 
-    app.use(pino({ transport: { target: 'pino-pretty' } }));
+  app.use(pino({ transport: { target: 'pino-pretty' } }));
 
-    app.use('/', (req, res) => {
-        res.json({
-            message: 'Hello Golova!',
-        });
+  app.get('/students', async (req, res) => {
+    const students = await getAllStudents();
+
+    res.status(200).json({
+      data: students,
     });
+  });
 
-    app.use((req, res, next) => {
-        res.status(404).json({
-            message: 'Route not found',
-        });
-    });
+  app.get('/students/:studentId', async (req, res) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
 
-    app.use((err, req, res, next) => {
-        res.status(500).json({
-            message: 'Something went wrong',
-            error: err.message,
-        });
-    });
+    if (!student) {
+      res.status(404).json({
+        message: 'Student not found',
+      });
+      return;
+    }
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+    res.status(200).json({
+      data: student,
     });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Route not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
