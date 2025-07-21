ccc5978455ae728b4600fe7b4f4d2d48893e353f
dan <81313913+danxczm@users.noreply.github.com>
2025-07-21 23:14:39 +0300

    feat: folders and errors

---
+++ src/controllers/students.js
@@ -0,0 +1,29 @@
+// src/controllers/students.js
+import createHttpError from 'http-errors';
+import { getAllStudents, getStudentById } from '../services/students.js';
+
+export const getStudentsController = async (req, res) => {
+  const students = await getAllStudents();
+
+  res.json({
+    status: 200,
+    message: 'Successfully found students!',
+    data: students,
+  });
+};
+
+export const getStudentByIdController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const student = await getStudentById(studentId);
+
+  if (!student) {
+    throw createHttpError(404, 'Student not found');
+  }
+
+  // Відповідь, якщо контакт знайдено
+  res.json({
+    status: 200,
+    message: `Successfully found student with id ${studentId}!`,
+    data: student,
+  });
+};
--- src/server.js
+++ src/server.js
@@ -4,7 +4,9 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllStudents, getStudentById } from './services/students.js';
+import studentsRouter from './routers/students.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -16,42 +18,17 @@ export const startServer = () => {
 
   app.use(pino({ transport: { target: 'pino-pretty' } }));
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
-
-    res.status(200).json({
-      data: students,
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello there!',
     });
   });
 
-  app.get('/students/:studentId', async (req, res) => {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
+  app.use(studentsRouter);
 
-    if (!student) {
-      res.status(404).json({
-        message: 'Student not found',
-      });
-      return;
-    }
+  app.use(notFoundHandler);
 
-    res.status(200).json({
-      data: student,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Route not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
